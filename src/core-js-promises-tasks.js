c8a22e9931d925aaba2583d89713527554a4aab1
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-03-21 15:19:08 +0300

    feat: add getAllResult solution

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -110,8 +110,11 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+async function getAllResult(promises) {
+  const arr = await Promise.allSettled(promises);
+  return arr.map((settled) => {
+    return settled.status === 'fulfilled' ? settled.value : null;
+  });
 }
 
 /**
